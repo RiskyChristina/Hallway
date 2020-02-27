85a320fac92501fb22e094352be32e96da968664
RiskyChristina <RiskyChristina@users.noreply.github.com>
2020-02-26 20:04:45 -0500

    Menu
    Menu

---
+++ Hallway/Assets/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("Pause");
+            if (GameIsPaused)
+            {
+                Resume();
+                Debug.Log("game is trying to resume");
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void MainM()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 0);
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.visible = true;
+    }
+}
---
+++ Hallway/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
+public class Player : MonoBehaviour
+{
+
+    public float timeLeft;
+    public TMP_Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetTimeText();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timeLeft -= Time.deltaTime;
+        SetTimeText();
+        if (timeLeft <= 0.01)
+        {
+            SetTimeText();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+            gameObject.SetActive(false);
+        }
+    }
+
+    void SetTimeText()
+    {
+        timeText.text = "Time: " + timeLeft.ToString("f0");
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            gameObject.SetActive(false);
+        }
+
+    }
+
+
+}
